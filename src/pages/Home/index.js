2b5ca297b9fb2fc9244130e7080930d672229e18
marinho-vinicius-clear <vinicius.souza.marinho.clear@gmail.com>
2023-05-23 23:29:32 -0300

    Made the login and registragion logic and started to do regiister the spendinds

---
+++ src/pages/Home/index.js
@@ -0,0 +1,41 @@
+import React, { useContext, useState } from 'react';
+import { View, Text, Button } from 'react-native';
+
+import { AuthContext } from '../../contexts/auth';
+import Header from '../../components/Header';
+import HistoricoList from '../../components/HistoricoList';
+
+import { Background, Container, Nome, Saldo, Title, List} from './styles';
+
+export default function Home() {
+  const [historico, setHistorico] = useState([
+    {key: '1', tipo: 'receita', valor: 1200},
+    {key: '2', tipo: 'despesa', valor: 200},
+    {key: '3', tipo: 'receita', valor: 40},
+    {key: '4', tipo: 'receita', valor: 89.62},
+    {key: '5', tipo: 'despesa', valor: 500},
+    {key: '6', tipo: 'despesa', valor: 310},
+  ]);
+
+  const { user } = useContext(AuthContext);
+
+ return (
+    <Background>
+      <Header/>
+      <Container>
+        <Nome>{user && user.nome}</Nome>
+        <Saldo>R$ 123,00</Saldo>
+      </Container>
+
+      <Title>Ultimas movimentações</Title>
+
+      <List
+      showsVerticalScrollIndicator={false}
+      data={historico}
+      keyExtractor={ item => item.key}
+      renderItem={ ({ item }) => ( <HistoricoList data={item} /> )}
+      />
+
+    </Background>
+  );
+}
